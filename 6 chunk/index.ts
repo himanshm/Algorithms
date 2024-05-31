d23e5bfe5fa8e628f048792bda2daa97d3a498c7
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-05-31 22:50:00 +0530

    Add another chunk method

--- 6 chunk/index.ts
+++ 6 chunk/index.ts
@@ -21,8 +21,24 @@ function chunk<T>(array: T[], size: number): T[][] {
   return chunked;
 }
 
+function chunkTwo<T>(array: T[], size: number): T[][] {
+  const chunked: T[][] = [];
+
+  for (let ele of array) {
+    const last = chunked[chunked.length - 1];
+    console.log(last);
+    if (!last || last.length === size) {
+      chunked.push([ele]);
+    } else {
+      last.push(ele);
+    }
+  }
+
+  return chunked;
+}
+
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 const size = 3;
 
-const result = chunk(numbers, size);
+const result = chunkTwo(numbers, size);
 console.log(result);
