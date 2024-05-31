dcc2ac4a037cc19d4df67bd78de4cdecd5d7732e
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-05-31 22:40:31 +0530

    Code to create chunks of arrays with given chunk size and array

--- 6 chunk/index.ts
+++ 6 chunk/index.ts
@@ -8,4 +8,21 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk<T>(array: T[], size: number): T[][] {
+  const chunked: T[][] = [];
+  let index = 0;
+
+  while (index < array.length) {
+    const arraySlice = array.slice(index, index + size);
+    chunked.push(arraySlice);
+    index += size;
+  }
+
+  return chunked;
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const size = 3;
+
+const result = chunk(numbers, size);
+console.log(result);
