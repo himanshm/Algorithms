c8179a9a962139f1398bd5c7c6e784395813f52f
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-06-01 22:46:54 +0530

    Check if the given year is a leap year

---
+++ 0 leapyear/index.ts
@@ -0,0 +1,13 @@
+function isLeap(year: number): boolean {
+  if (year % 4 !== 0) {
+    return false;
+  } else if (year % 100 !== 0) {
+    return true;
+  } else if (year % 400 !== 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+console.log(isLeap(2024));
